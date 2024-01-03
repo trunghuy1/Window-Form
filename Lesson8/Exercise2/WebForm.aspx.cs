06020401e9f6d4a09b95a0a6379802a19e2c31f0
Keii251 <blfofflower@gmail.com>
2024-01-03 21:00:37 +0700

    update lession

---
+++ Lesson8/Exercise2/WebForm.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise2
+{
+	public partial class WebForm : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+			WebService webService = new WebService();
+			int a = 5;
+			int b = 6;
+			Label1.Text = "Output of WebService: " + webService.Add(a,b).ToString();
+			Label1.Text = "Output of WebService: " + webService.Subtract(a, b).ToString();
+			Label1.Text = "Output of WebService: " + webService.Multiply(a, b).ToString();
+			Label1.Text = "Output of WebService: " + webService.Divide(a, b).ToString();
+		}
+	}
+}
---
+++ Lesson9/Exercise1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			var db = new Lesson9Entities();
+			var select = from s in db.Students select s;
+			String st = "";
+			foreach (var item in select)
+			{
+				st = st + "ID: " + item.studentID.ToString() + System.Environment.NewLine;
+				st = st + "Name: " + item.studentName.ToString() +
+				System.Environment.NewLine;
+				st = st + "Gender: " + item.studentGender.ToString() +
+				System.Environment.NewLine;
+				st = st + "Address: " + item.Address_.ToString() + System.Environment.NewLine;
+			}
+			this.textBox1.Text = st;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var db = new Lesson9Entities();
+			Student aStudent;
+			if (db.Students.Find(5) == null)
+			{
+				aStudent = new Student();
+				aStudent.studentID = 4;
+				aStudent.studentName = "Nguyen Tri Dung";
+				aStudent.studentGender = "Male";
+				aStudent.Address_ = " 11 Le Lai";
+				db.Students.Add(aStudent);
+				db.SaveChanges();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			var db = new Lesson9Entities();
+			Student aStudent = new Student();
+			aStudent = db.Students.Where(d => d.studentID == 3).First() as Student;
+			aStudent.studentName = "Nguyen Van Linh";
+			db.SaveChanges();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			var db = new Lesson9Entities();
+			Student aStudent;
+			aStudent = db.Students.Where(d => d.studentID == 5).First() as Student;
+			db.Students.Remove(aStudent);
+			db.SaveChanges();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			using (Lesson9Entities db = new Lesson9Entities())
+			{
+				Student aStudent;
+				// add
+				aStudent = new Student();
+				aStudent.studentID = 5;
+				aStudent.studentName= "Nguyen Tri Dung";
+				aStudent.studentGender = "Male";
+				aStudent.Address_ = " 11 Le Lai";
+				db.Students.Add(aStudent);
+				// update
+				aStudent = db.Students.Where(d => d.studentID == 5).First() as Student;
+				aStudent.studentName = "Nguyen Van Linh";
+				//delete
+				aStudent = db.Students.Where(d => d.studentID == 5).First() as Student;
+				db.Students.Remove(aStudent);
+				db.SaveChanges();
+			}
+		}
+	}
+}
