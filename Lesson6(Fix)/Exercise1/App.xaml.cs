bfbdff169dad23db8d46262a02fd2aab86ea689b
Keii251 <blfofflower@gmail.com>
2024-01-04 08:07:31 +0700

    fix lession6

---
+++ Lesson6(Fix)/Exercise1/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MVVMInWPF.ViewModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Exercise1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Exercise1.View.MainPage window = new Exercise1.View.MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
---
+++ Lesson6(Fix)/Exercise2/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Exercise2.View_Model;
+
+namespace Exercise2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            View.MainPage person = new View.MainPage();
+            PersonViewModel personViewModel = new PersonViewModel();
+            person.DataContext = personViewModel;
+            person.Show();
+        }
+    }
+}
